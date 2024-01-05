7896f0c90474bb4b85a1d41fa2874e513aff11ed
Kucher1995 <sashadovgun@gmail.com>
2024-01-05 19:21:38 +0300

    fixed

--- brain_games/games/br_even.py
+++ brain_games/games/br_even.py
@@ -5,20 +5,19 @@ from brain_games import engine
 def main():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     x = 0
-    while x <= 3:
+    while x < 3:
         z = randrange(1, 50)
         engine.question(z)
         engine.answer()
         if z % 2 == 0 and engine.ans == 'yes' or z % 2 != 0 and engine.ans == 'no':
-            if x == 2:
-                engine.congratulations()
-                break
-            else:
-                engine.correct()
+            engine.correct()
             x += 1
         elif z % 2 == 0 and engine.ans != 'yes':
             engine.wrong_answer(engine.ans, 'yes')
             break
-        elif z % 2 != 0 and engine.ans != 'no':
+        else:
+            z % 2 != 0 and engine.ans != 'no'
             engine.wrong_answer(engine.ans, 'no')
             break
+    else:
+        engine.congratulations()
