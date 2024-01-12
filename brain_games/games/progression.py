c9448a83ee9f57d9da587ba3619dd044d1d34c39
Kucher1995 <sashadovgun@gmail.com>
2024-01-12 14:45:36 +0300

    попытка№4

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,20 +5,6 @@ def exercise():
     print('What number is missing in the progression?')
 
 
-def random_progression():
-    num1 = randint(1, 10)
-    num2 = randint(100, 120)
-    n = randint(2, 9)
-    numbers = []
-    for i in range(num1, num2, n):
-        numbers.append(i)
-    numbers.sort()
-    correct_answer = numbers[n]
-    numbers[n] = ".."
-    string = " ".join(map(str, numbers[0:10]))
-    return correct_answer, string
-
-
 def generate_question():
     num1 = randint(1, 10)
     num2 = randint(100, 120)
