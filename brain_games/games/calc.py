09619c7168cee22c77ce9c3d33926b94bd56a796
Kucher1995 <82833359+Kucher1995@users.noreply.github.com>
2024-01-19 18:46:24 +0300

    доработка

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -21,7 +21,7 @@ def generate_question():
     num1 = randrange(20, 50)
     num2 = randrange(1, 20)
     massiv = ['-', '+', '*']
-    random_expression = massiv[randrange(0, 3)]
-    random_expression = f'{num1} {random_expression} {num2}'
-    correct_answer = get_result(num1, num2, random_expression)
+    random_operation = massiv[randrange(0, 3)]
+    random_expression = f'{num1} {random_operation} {num2}'
+    correct_answer = get_result(num1, num2, random_operation)
     return random_expression, correct_answer
